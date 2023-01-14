d88f5de2065997102f216e4d81d8d8e74ddf7229
Adham-Niazy <adhamniazy@gmail.com>
2023-01-14 23:42:21 +0200

    Pinning & Highlight Animations for first two sections

--- main.js
+++ main.js
@@ -3,4 +3,54 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 import './styles/index.css';
 
-gsap.registerPlugin(ScrollTrigger); 
+gsap.registerPlugin(ScrollTrigger);
+
+// Pin the first section
+const IntroSectionTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.hero__page',
+    start: '0%',
+    end: '100%',
+    pin: true,
+    pinSpacing: false
+  }
+})
+
+// Highlight Smoke Section
+const smokeSectionTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.smoke__page',
+    scrub: true,
+    start: "-40%",
+    end: "50%"
+  }
+});
+
+smokeSectionTimeline.fromTo(
+  '.highlight',
+  {
+    color: 'rgba(255, 255, 255, 0.4'
+  },
+  {
+    color: 'rgba(255, 255, 255, 1)',
+    stagger: 1
+  }
+)
+
+// Remove Highlight After Passing
+const smokeSectionTimelineRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.smoke__page',
+    scrub: true,
+    start: "-20%",
+    end: "70%"
+  }
+});
+
+smokeSectionTimelineRemove.to(
+  '.highlight',
+  {
+    color: 'rgba(255, 255, 255, 0.4)',
+    stagger: 1
+  }
+)
